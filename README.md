6b59c4ca3ed8f3927edbed176d131e1ad527c682
Mark Kimsal <metrofindings@gmail.com>
2022-02-12 08:12:01 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# INSTALLATION
+
+run `update_versions.sh` from the `vendor_dompdf` folder.
+
+run `composer run new-env` to install a new environment
---
+++ vendor_dompdf/update_versions.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+for version in "1.2.0" "1.1.1"
+do
+	versiondashes=$(echo -n "$version" | sed -e "s/\./\-/g")
+	if [ -d "dompdf-$version" ]; then
+		echo "directory exists dompdf-$version"
+	else
+		echo "directory does not exist dompdf-$version"
+		echo "downloading version $version..."
+		wget "https://github.com/dompdf/dompdf/releases/download/v$version/dompdf_$versiondashes.zip"
+		unzip "dompdf_$versiondashes.zip"
+		rm "dompdf_$versiondashes.zip"
+		mv "dompdf" "dompdf-$version"
+		echo "donwloaded version dompdf_$version.zip to dompdf-$version/"
+	fi
+done
